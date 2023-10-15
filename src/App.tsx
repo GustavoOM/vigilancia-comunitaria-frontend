bf1162c2db8226be2ec7465af17a2442f3bbdb51
Joao Otavio <joao0otavio3@gmail.com>
2023-10-15 12:43:11 -0300

    adiciona react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import "./App.css"
 
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/Cadastro";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<h1>Home</h1>} path="/" />
+          <Route element={<Login />} path="/login"/>
+          <Route element={<Cadastro />} path="/cadastro"/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
