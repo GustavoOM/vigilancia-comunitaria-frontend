8721454a934d77e45bec63e4055abe9c96ed1a28
lminoru <leo.minoru@hotmail.com>
2023-10-19 04:36:49 -0300

    rota para acessar o feed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css"
 
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/Cadastro";
+import Feed from "./pages/Feed/Feed";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<h1>Home</h1>} path="/" />
-          <Route element={<Login />} path="/login"/>
-          <Route element={<Cadastro />} path="/cadastro"/>
+          <Route element={<Login />} path="/login" />
+          <Route element={<Cadastro />} path="/cadastro" />
+          <Route element={<Feed />} path="/feed" />
         </Routes>
       </BrowserRouter>
     </>
