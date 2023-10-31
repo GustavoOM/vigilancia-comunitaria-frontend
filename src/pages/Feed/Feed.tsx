44532cf3e7ec5c2f57fd0deea434c9373660ced6
lminoru <leo.minoru@hotmail.com>
2023-10-30 23:55:18 -0300

    feed integrado ao backend

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,32 +1,60 @@
 import Postagem from "../../components/Postagem/Postagem";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import { useEffect, useState } from 'react';
 import { Stack } from "@mui/material"
 
-function ComponenteExemplo() {
-    const postagens = [
-        { urlImgPerfil: "https://picsum.photos/40/40", nomeUsuario: "Simone_1", tipo: "Ocorrência - ICMC", descricao: "Árvore tombada na saída da matemática", urlImagem: "https://picsum.photos/1170/720" },
-        { urlImgPerfil: "https://picsum.photos/80/80", nomeUsuario: "Fuzeto", tipo: "Alerta - ICMC", descricao: "Sem internet em todo o instituto de matemática e computação, eduroam caiu hoje cedo!", urlImagem: "https://picsum.photos/1171/721" },
-        { urlImgPerfil: "https://picsum.photos/120/120", nomeUsuario: "Anônimo", tipo: "Denúncia - ICMC", descricao: "Pessoas estranhas entraram em grupo na noite de 02/10", urlImagem: "https://picsum.photos/1172/722" }
+function Feed() {
+    const postagensMock = [
+        { urlImgPerfil: "https://picsum.photos/40/40", nameAuthor: "Simone_1", type: "Ocorrência - ICMC", content: "Árvore tombada na saída da matemática", images: "https://picsum.photos/1170/720" },
+        { urlImgPerfil: "https://picsum.photos/80/80", nameAuthor: "Fuzeto", type: "Alerta - ICMC", content: "Sem internet em todo o instituto de matemática e computação, eduroam caiu hoje cedo!", images: "https://picsum.photos/1171/721" },
+        { urlImgPerfil: "https://picsum.photos/120/120", nameAuthor: "Anônimo", type: "Denúncia - ICMC", content: "Pessoas estranhas entraram em grupo na noite de 02/10", images: "https://picsum.photos/1172/722" }
     ]
 
+    const [postagens, setPostagensa] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+            const requestConfig = {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem('vigilancia-token'),
+                    "Content-Type": "application/json"
+                },
+            };
+
+            const response = await fetch(`${apiBaseUrl}/post`, requestConfig);
+            const result = await response.json();
+
+            setPostagensa(result);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+
     return (
-        
+
         <Stack justifyContent="center" alignItems="center" spacing={1}>
-            <Header/>
+            <Header />
             {postagens.map((postagem, index) => (
                 <Postagem
                     key={index}
-                    urlImgPerfil={postagem.urlImgPerfil}
-                    nomeUsuario={postagem.nomeUsuario}
-                    tipo={postagem.tipo}
-                    descricao={postagem.descricao}
-                    urlImagem={postagem.urlImagem}
+                    urlImgPerfil={/*postagem['urlImgPerfil'] */ "https://picsum.photos/120/120"}
+                    nomeUsuario={postagem['nameAuthor']}
+                    tipo={postagem['type']}
+                    descricao={postagem['content']}
+                    urlImagem={/*postagem['images']*/"https://picsum.photos/1170/720"}
                 />
             ))}
-            <Footer/>
+            <Footer />
         </Stack>
     )
 }
 
-export default ComponenteExemplo;
+export default Feed;
