cf41ec820c93344f03786f47eb289ccf1b368da5
Eduardo Maciel <mrgamex49@gmail.com>
2023-11-01 12:13:33 -0300

    fix(feed): fix feed color and login Bearer token

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,71 +1,73 @@
-import Postagem from "../../components/Postagem/Postagem";
-import Header from "../../components/Header/Header";
+import { Stack } from "@mui/material";
+import { useEffect, useState } from "react";
 import Footer from "../../components/Footer/Footer";
-import { useEffect, useState } from 'react';
-import { Stack } from "@mui/material"
+import Header from "../../components/Header/Header";
+import Postagem from "../../components/Postagem/Postagem";
 
 function Feed() {
-    const postagensMock = [
-        { urlImgPerfil: "https://picsum.photos/40/40", nameAuthor: "Simone_1", type: "Ocorrência - ICMC", content: "Árvore tombada na saída da matemática", images: "https://picsum.photos/1170/720" },
-        { urlImgPerfil: "https://picsum.photos/80/80", nameAuthor: "Fuzeto", type: "Alerta - ICMC", content: "Sem internet em todo o instituto de matemática e computação, eduroam caiu hoje cedo!", images: "https://picsum.photos/1171/721" },
-        { urlImgPerfil: "https://picsum.photos/120/120", nameAuthor: "Anônimo", type: "Denúncia - ICMC", content: "Pessoas estranhas entraram em grupo na noite de 02/10", images: "https://picsum.photos/1172/722" }
-    ]
+  const [postagens, setPostagens] = useState([]);
 
-    const [postagens, setPostagens] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const fetchData = async () => {
+    try {
+      const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+      const requestConfig = {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("vigilancia-token"),
+          "Content-Type": "application/json",
+        },
+      };
 
-    const fetchData = async () => {
-        try {
-            const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-            const requestConfig = {
-                method: "GET",
-                headers: {
-                    "Authorization": "Bearer " + localStorage.getItem('vigilancia-token'),
-                    "Content-Type": "application/json"
-                },
-            };
+      const response = await fetch(`${apiBaseUrl}/post`, requestConfig);
+      const result = await response.json();
 
-            const response = await fetch(`${apiBaseUrl}/post`, requestConfig);
-            const result = await response.json();
+      setPostagens(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-            setPostagens(result);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+  return (
+    <>
+      <Header />
 
+      {postagens?.length != 0 ? (
+        <Stack justifyContent="center" alignItems="center" spacing={1}>
+          {postagens.map((postagem, index) => (
+            <Postagem
+              key={index}
+              urlImgPerfil={
+                /*postagem['urlImgPerfil'] */ "https://picsum.photos/120/120"
+              }
+              nomeUsuario={postagem["nameAuthor"]}
+              tipo={postagem["type"]}
+              descricao={postagem["content"]}
+              urlImagem={
+                /*postagem['images']*/ "https://picsum.photos/1170/720"
+              }
+            />
+          ))}
+        </Stack>
+      ) : (
+        <div
+          style={{
+            minHeight: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h6> Não há postagens. Crie a sua!</h6>
+        </div>
+      )}
 
-    return (
-
-        <>
-            <Header />
-
-            {postagens?.length != 0 ?
-                <Stack justifyContent="center" alignItems="center" spacing={1} bgcolor={"red"}>
-                    {postagens.map((postagem, index) => (
-                        <Postagem
-                            key={index}
-                            urlImgPerfil={/*postagem['urlImgPerfil'] */ "https://picsum.photos/120/120"}
-                            nomeUsuario={postagem['nameAuthor']}
-                            tipo={postagem['type']}
-                            descricao={postagem['content']}
-                            urlImagem={/*postagem['images']*/"https://picsum.photos/1170/720"}
-                        />
-                    ))}
-                </Stack >
-                : (
-                    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <h6> Não há postagens. Crie a sua!</h6>
-                    </div>
-                )
-            }
-
-            <Footer />
-        </>
-    )
+      <Footer />
+    </>
+  );
 }
 
 export default Feed;
